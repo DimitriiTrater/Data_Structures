7b03ef939dbe0f9751e84103ee7732c607d2d2ae
DimitriiTrater <dima1603040@gmail.com>
2022-06-25 03:29:43 +0500

    Add delete() string 68 Add _def() string 30

---
+++ Data_Structures/DBList.py
@@ -0,0 +1,93 @@
+class DbList:
+    class Node:
+        element = None
+        next_node = None
+        prev_node = None
+
+        def __init__(self, element, next_node=None, prev_node=None) -> None:
+            self.element = element
+            self.next_node = next_node
+            self.prev_node = prev_node
+
+    head = None
+    tail = None
+    length = 0
+
+    def append(self, element):
+        self.length += 1
+        if not self.head:
+            self.head = self.Node(element)
+            return element
+        elif not self.tail:
+            self.tail = self.Node(element, None, self.head)
+            self.head.next_node = self.tail
+            return element
+        else:
+            self.tail = self.Node(element, None, self.tail)
+            self.tail.prev_node.next_node = self.tail
+            return element
+
+    def _del(self, index, reverse=False):
+        if index == 0:
+            el = self.head.element
+            self.head = self.head.next_node
+            self.head.prev_node = None
+            return el
+        elif index == self.length - 1:
+            el = self.tail.element
+            self.tail = self.tail.prev_node
+            self.tail.next_node = None
+            return el
+        elif reverse:
+            i = self.length - 1
+            node = self.tail
+
+            while i != index:
+                node = node.prev_node
+                i -= 1
+
+            el = node.element
+            node.prev_node.next_node, node.next_node.prev_node = node.next_node, node.prev_node
+            del node
+
+            return el
+        else:
+            i = 0
+            node = self.head
+
+            while i != index:
+                node = node.next_node
+                i += 1
+
+            el = node.element
+            node.prev_node.next_node, node.next_node.prev_node = node.next_node, node.prev_node
+            del node
+
+            return el
+
+    def delete(self, index):
+        if self.head:
+            if index > self.length // 2:
+                el = self._del(index, reverse=True)
+            elif index <= self.length // 2:
+                el = self._del(index, reverse=False)
+            self.length -= 1
+            return el
+
+    def __str__(self):
+        node = self.head
+        line = '['
+        while node.next_node:
+            line += str(node.element) + ','
+            node = node.next_node
+        line += str(node.element) + ']'
+        return line
+
+
+if __name__ == '__main__':
+    a = DbList()
+    a.append(7)
+    a.append(3)
+    a.append(5)
+    a.delete(1)
+    print(a, a.length)
